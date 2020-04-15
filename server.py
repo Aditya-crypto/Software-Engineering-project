14f340bc1457cfc7b421dd42e9b72336932adef1
deeksha-123 <55048784+deeksha-123@users.noreply.github.com>
2020-04-16 01:53:00 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,30 @@
+import socket   
+import signal, os    
+import sounddevice as sd
+import numpy as np 
+from math import pi 
+
+######################################
+s = socket.socket()          
+print ("Socket successfully created")
+port = 12346             
+s.bind(('', port))         
+print ("socket binded to %s" %(port))  
+s.listen(5)      
+print ("socket is listening") 
+c, addr = s.accept()  
+############################################
+
+msg=c.recv(4096)
+print(msg)
+while(msg):
+   fs=16000
+   n=np.arange(0,3,1/fs)
+   f=1000
+   x=np.sin(2*pi*f*n)
+   sd.play(x,fs)
+
+output = 'Thank you for connecting'
+c.sendall(output.encode('utf-8')) 
+
+c.close() 
