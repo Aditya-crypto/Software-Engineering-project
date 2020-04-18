d31ae06017a4ce2330292a387507c6bb0a73b370
Aditya Gupta <54924007+Aditya-crypto@users.noreply.github.com>
2020-04-18 10:10:50 +0530

    Update and rename map.py to BuildMap.py

---
+++ BuildMap.py
@@ -0,0 +1,16 @@
+
+# Reading database.txt file from nas and storing the contents in content list
+
+def MapBuilder():
+    with open('database.txt') as f:
+        content = f.read().splitlines()   
+
+    # creating a dictionary to map camera id's with nearest police stations
+    m = dict()
+    for i in range(len(content)):
+        cam_id = content[i].split(':')
+        m[int(cam_id[0])] = list()
+        nearby_stations = cam_id[1].split('||')
+        for i in range(len(nearby_stations)):
+            m[int(cam_id[0])].append(nearby_stations[i])
+	return m
