518fe72a3c9de3f1a1f264f06a99cd23a604067b
Richa0095 <richa0095@gmail.com>
2020-04-22 16:56:23 +0530

    Update ListGenerator.py

--- ListGenerator.py
+++ ListGenerator.py
@@ -1,5 +1,6 @@
 def CreateList():
 	with open('NAS_data.txt') as f:
 		content = f.readline()
+		content = content.rstrip("\n")
                 nas_info = content.split(";")
        return nas_info
