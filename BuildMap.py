3fecab99f62b971bee72d9e1b672dd6acd2eab0a
Aditya Gupta <54924007+Aditya-crypto@users.noreply.github.com>
2020-04-22 20:15:35 +0530

    Update BuildMap.py

--- BuildMap.py
+++ BuildMap.py
@@ -1,4 +1,4 @@
-# Reading database.txt file from nas and storing the contents in content list
+# Reading database.txt file from nfs and storing the contents in content list
 def MapBuilder():
 	with open('database.txt') as f:
 		content = f.read().splitlines()   
@@ -11,4 +11,16 @@ def MapBuilder():
                 nearby_stations = cam_id[1].split('||')
                 for i in range(len(nearby_stations)):
 			m[int(cam_id[0])].append(nearby_stations[i])
+	return m
+
+# Reading location.txt file from nfs and storing the contents in content list
+def LocationMapBuilder():
+	with open('location.txt') as f:
+		content = f.read().splitlines()   
+
+	# creating a dictionary to map camera id's with their location
+	m = dict()
+	for i in range(len(content)):
+		cam_id = content[i].split(':')
+		m[int(cam_id[0])] = cam_id[1]
 	return m
