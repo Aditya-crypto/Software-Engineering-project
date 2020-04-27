39d390fb871eeca82428aa44fe88374dbf73b44c
Divyani Indurkhya <divyanindurkhya01@gmail.com>
2020-04-27 15:31:04 +0530

    Add files via upload

---
+++ ImageExtraction_googlecolabrun.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
+
+def take_photo(filename, quality=0.8):
+  js = Javascript('''
+    async function takePhoto(quality) {
+      const div = document.createElement('div');
+      const capture = document.createElement('button');
+      capture.textContent = 'Capture';
+      div.appendChild(capture);
+
+      const video = document.createElement('video');
+      video.style.display = 'block';
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+      document.body.appendChild(div);
+      div.appendChild(video);
+      video.srcObject = stream;
+      await video.play();
+
+      // Resize the output to fit the video element.
+      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+      // Wait for Capture to be clicked.
+      // await new Promise((resolve) => capture.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+      div.remove();
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+    ''')
+  display(js)
+  data = eval_js('takePhoto({})'.format(quality))
+  binary = b64decode(data.split(',')[1])
+  with open(filename, 'wb') as f:
+    f.write(binary)
+  return filename
+
+
+# In[12]:
+
+
+from IPython.display import Image
+import time
+try:
+  for i in range(10):
+    timestamp = time.strftime('%H:%M:%S')
+    hr=timestamp.split(':')
+    s=str(hr[0]+'.'+hr[1]+'.'+hr[2])
+    serialno="12345678"
+    directory="/content/sample_data/UNPROCESSED/"+serialno+"_"+s+".png"
+    filename = take_photo(directory)
+    print('Saved to {}'.format(filename))
+    time.sleep(5)
+  # Show the image which was just taken.
+  display(Image(filename))
+except Exception as err:
+  # Errors will be thrown if the user does not have a webcam or if they do not
+  # grant the page permission to access it.
+  print(str(err))
+
+
+
+
+
+
+
