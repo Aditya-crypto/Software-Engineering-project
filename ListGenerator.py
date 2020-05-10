a6b7d1f30478dca72b1e3f5525393ec0296011a5
Aditya Gupta <54924007+Aditya-crypto@users.noreply.github.com>
2020-05-10 16:44:45 +0530

    Delete ListGenerator.py

--- ListGenerator.py
+++
@@ -1,6 +0,0 @@
-def CreateList():
-	with open('NAS_data.txt') as f:
-		content = f.readline()
-		content = content.rstrip("\n")
-                nas_info = content.split(";")
-       return nas_info
